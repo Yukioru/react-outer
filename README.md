02dd03ee4d049e42424ed5e650c6393be29afdde
Andrey Kondalov <obt195@gmail.com>
2018-04-30 19:23:26 +0400

    renaming, tag Fragment defaults

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # react-outside-component
 React component for handling outside clicks. Inspired by [reactstrap](https://github.com/reactstrap/reactstrap). 
 
-[![npm version](https://img.shields.io/npm/v/react-outside-component.svg?style=flat-square)](https://www.npmjs.com/package/react-outside-component)
+[![npm version](https://img.shields.io/npm/v/react-outer.svg?style=flat-square)](https://www.npmjs.com/package/react-outer)
 
 ## Installation
 
 ```sh
-yarn add react-outside-component
+yarn add react-outer
 ```
 
 ## Api
@@ -14,11 +14,15 @@ yarn add react-outside-component
 ### Properties
 
 ```js
-OutsideComponent.propTypes = {
-    tag: PropTypes.string, // default 'div'
-    isOpen: PropTypes.bool, // default true
-    children: PropTypes.node,
-    onClickOutside: PropTypes.func.isRequired,
+Outer.propTypes = {
+  tag: PropTypes.oneOfType([ // default Fragment
+    PropTypes.string,
+    PropTypes.symbol,
+    PropTypes.element,
+  ]),
+  isOpen: PropTypes.bool, // default true
+  children: PropTypes.node,
+  onClickOutside: PropTypes.func.isRequired,
 };
 ```
 
@@ -26,31 +30,33 @@ OutsideComponent.propTypes = {
 
 ```js
 import React, { Component } from 'react';
-import Outside from 'react-outside-component';
+import Outside from 'react-outer';
 
 export default class CustomDropdown extends Component {
     
-    constructor(props) {
-        super(props);
-        
-        this.handleOutsideClick = this.handleOutsideClick.bind(this);
-    }
-    
-    handleOutsideClick(e) {
-        // ...
-    }
-    
-    render() {
-        return (
-            <Outside tag="ul" className="dropdown-menu" onClickOutside={this.handleOutsideClick}>
-                <li>Items..</li>
-            </Outside>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+  }
+  
+  handleOutsideClick(e) {
+    // ...
+  }
+  
+  render() {
+    return (
+      <Outside
+        tag="ul"
+        className="dropdown-menu"
+        onClickOutside={this.handleOutsideClick}
+      >
+        <li>Items..</li>
+      </Outside>
+    );
+  }
 }
 ``` 
 
 ## License
 
 MIT
-
--- src/index.js
+++ src/index.js
@@ -1,77 +1,83 @@
-import omit from 'lodash/omit';
-import ReactDOM from 'react-dom';
+import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import omit from 'lodash/omit';
 
-class OutsideComponent extends Component {
+class Outer extends PureComponent {
+  DOMEvents = ['click'];
+  _isMounted;
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.handleDocumentClick = this.handleDocumentClick.bind(this);
+    
+  }
+  
+  static defaultProps = {
+    tag: Fragment,
+    isOpen: true,
+  };
 
-        this.handleDocumentClick = this.handleDocumentClick.bind(this);
+  static propTypes = {
+    tag: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.symbol,
+      PropTypes.element,
+    ]),
+    isOpen: PropTypes.bool,
+    children: PropTypes.node,
+    onClickOutside: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    this._isMounted = true;
+    if (this.props.isOpen) {
+      this.addEvents();
     }
+  }
 
-    static propTypes = {
-        tag: PropTypes.string,
-        isOpen: PropTypes.bool,
-        children: PropTypes.node,
-        onClickOutside: PropTypes.func.isRequired,
-    };
-
-    static defaultProps = {
-        tag: 'div',
-        isOpen: true,
-    };
-
-    DOMEvents = ['click'];
-
-    componentDidMount() {
-        if (this.props.isOpen) {
-            this.addEvents();
-        }
+  componentDidUpdate({ isOpen }) {
+    if (this.props.isOpen !== isOpen) {
+      this.handleProps();
     }
+  }
 
-    componentDidUpdate({ isOpen }) {
-        if (this.props.isOpen !== isOpen) {
-            this.handleProps();
-        }
+  componentWillUnmount() {
+    this._isMounted = false;
+    this.removeEvents();
+  }
+
+  handleProps() {
+    if (!this._isMounted) return;
+    (this.props.isOpen)
+      ? this.addEvents()
+      : this.removeEvents()
+  }
+
+  handleDocumentClick(e) {
+    const container = ReactDOM.findDOMNode(this);
+    if (!container.contains(e.target)) {
+      this.props.onClickOutside(e);
     }
+  }
 
-    componentWillUnmount() {
-        this.removeEvents();
-    }
+  addEvents() {
+    this.DOMEvents.forEach(event =>
+      document.addEventListener(event, this.handleDocumentClick, true)
+    );
+  }
 
-    handleProps() {
-        (this.props.isOpen) ? this.addEvents() : this.removeEvents();
-    }
+  removeEvents() {
+    this.DOMEvents.forEach(event =>
+      document.removeEventListener(event, this.handleDocumentClick, true)
+    );
+  }
 
-    handleDocumentClick(e) {
-        const container = ReactDOM.findDOMNode(this);
-
-        if (!container.contains(e.target)) {
-            this.props.onClickOutside(e);
-        }
-    }
-
-    addEvents() {
-        this.DOMEvents.forEach(event =>
-            document.addEventListener(event, this.handleDocumentClick, true)
-        );
-    }
-
-    removeEvents() {
-        this.DOMEvents.forEach(event =>
-            document.removeEventListener(event, this.handleDocumentClick, true)
-        );
-    }
-
-    render() {
-        const { tag: Tag, children, ...props } = omit(this.props, ['isOpen', 'onClickOutside']);
-
-        return <Tag {...props}>{children}</Tag>;
-
-    }
+  render() {
+    const { tag: Tag, children, ...props } = omit(this.props, ['isOpen', 'onClickOutside']);
+    return <Tag {...props}>{children}</Tag>;
+  }
 
 }
 
-export default OutsideComponent;
+export default Outer;
